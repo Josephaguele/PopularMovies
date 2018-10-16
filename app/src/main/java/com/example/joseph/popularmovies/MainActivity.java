c59fe4427b0d4627ca3d783a6a49d0f0f4557a4e
Joseph <osekuemen@gmail.com>
2018-10-16 15:35:33 +0100

    Adding detail activity

--- app/src/main/java/com/example/joseph/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/joseph/popularmovies/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.joseph.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -32,7 +35,20 @@ public class MainActivity extends AppCompatActivity {
         final GridView gridView = (GridView) findViewById(R.id.listview);
         gridView.setAdapter(adapter);
 
+        // this onclick method opens any item clicked.
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // calling intent to launch detail activity
+                launchDetailActivity(position);
+            }
+        });
 
+    }
 
+    // This method launches the detail activity
+    private void launchDetailActivity(int position) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
